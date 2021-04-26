44efbe02fbc1223ee9ecac6a509c49d6d6303315
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-26 18:23:01 -0400

    Create 01-installing.js

---
+++ express/01-installing.js
@@ -0,0 +1,35 @@
+mkdir flashcards
+cd flashcards
+npm init -y //use this with y to get everything we needed
+{
+  "name": "flashcards",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
+}
+
+npm install express --save  //save and add to the json file for the dependency
+
+
+//Install express, version 4.15.2, and save it to the package.json file.
+npm install express@4.15.2 --save
+
+
+//questions
+Web frameworks take care of low level code, allowing you to focus on what makes your app different from anyone else's.
+
+In order to use node modules in a file, they need to be _____ using the _____ method
+required, require
+
+To start a node project in a directory without a package.json file run the following command:
+npm init
+
+What is a word for solutions to common problems in software?
+Patterns
+  
