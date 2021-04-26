34ff4d0a362d4f0543e2c74a79b1fa49ac6a8941
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-26 16:59:01 -0400

    Delete node.js directory

--- node.js/accessing-function.js
+++
@@ -1,9 +0,0 @@
-//accessing functionality from another file
-
-function sayGreeting() {
-  console.log("Hello World"); }
-module.exports.say = sayGreeting; 
-// need this module.export outside the function in order to require from another file
----------
-const greeting = require('./greeting'); //same directory file
-greeting.say()
