8f1c3921159127f9a00e6d625bc3d4bc3f7f8735
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-28 08:31:03 -0400

    Create 05-review-questions.js

---
+++ asynchronous/05-review-questions.js
@@ -0,0 +1,20 @@
+This approach to asynchronous programming provides a then() method that you can use to chain together a series of asynchronous functions:
+promises
+
+This approach to asynchronous programming allows you to specify when JavaScript should wait for a function to return a result before moving on to the next line of code.
+async/await
+
+This approach to asynchronous programming uses nested functions to ensure asynchronous tasks are handled in the desired order:
+callback
+
+What is an example of an asynchronous task?
+Requesting information from a database
+
+The await keyword can not be used inside of any regular JavaScript function
+The await keyword must be used inside of a function flagged async
+
+What is a potential pitfall of using callbacks to handle asynchronous tasks?
+many nested functions can cause a pyramid of doom which can make the logic difficult to foolow
+  
+
+  
