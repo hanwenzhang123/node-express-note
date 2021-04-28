beb5fafeaa29aa9b378ba1c9a121e2be29fa8263
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-28 14:05:16 -0400

    Update 04-middleware.js

--- asynchronous/04-middleware.js
+++ asynchronous/04-middleware.js
@@ -1,16 +1,18 @@
-function asyncHandler(cb){
-  return async (req, res, next) => {
-    try{
-      await cb (req, res, next);
+function asyncHandler(cb){    //create a function that wraps around our normal route handling function
+  return async (req, res, next) => {    //this function we can pass all the code we would normally pass
+    try{    //try block, we will await whatever function we have passed to the asyncHandler function with the normal route handling parameters
+      await cb (req, res, next);  
     } catch(err){
       res.render('error', {error: err});
-    }
+    } // render our err page
   }
 }
+// we do not need to include this err code in every single route
+// you can delete the error handling code in the route
 
-// async/await
+//ASYNC/AWAIT
+
 app.get('/', asyncHandler(async (req, res) => {
-  const users = await getUsers();
-// throw new Err('It broke')    throw error check
+  const users = await getUsers(); // throw new Err('It broke')  -  throw error to check
   res.render('index', {title: 'Users', users: users.users});
 }));
