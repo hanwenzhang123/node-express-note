cf2f4c0ae655aecf4cf6e61633df87ee5a3d91ca
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-28 14:35:49 -0400

    Create 01-intro-rest-api.js

---
+++ express-api/01-intro-rest-api.js
@@ -0,0 +1,45 @@
+Web Application 
+- serverside
+- clientside
+
+REST API
+when you request for a specific piece of data/information, the application responds only with that data, typically JSON
+
+Client makes request from URL
+client - application requesting informing from a server
+
+REST - representational state transfer
+how the server can respond the request from a client 
+
+CRUD Operations
+Create, Read, Update, Delete
+
+REST API
+GET - read/view
+POST - create
+PUT - update
+DELETE - delete
+
+API - takes in request, and responds with JSON objects
+
+When we  write an api, we are essentially proagmming responses to various HTTP requests for resources
+through code, we say to our express application, when a client makes a get request to the resouces/movies, respond with a JSON object containing a list of movies.
+
+
+//Questions
+What is one benefit of building an application using a REST API?
+  because it only returns data, you can use a REST API as the backend for numerous application in flexible ways
+
+What best describes what an API does?
+  Take in requests and responds with JSON objects
+
+Which HTTP verbs match up with the actions that can be performed on data with an API? (create, read, update, delete)
+  POST, GET, PUT, DELETE
+
+What does CRUD stand for?
+  Create, Read, Update, Delete
+
+How does a REST API differ from a traditional serverside application?
+  A REST API application responds to requests with data such as JSON; a tradtional serverside application responds to requests with HTML.
+
+   
