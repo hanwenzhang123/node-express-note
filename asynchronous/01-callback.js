b6a39d155588fa717d2b56cbf7f68116c9ed22e6
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-28 13:42:27 -0400

    Update 01-callback.js

--- asynchronous/01-callback.js
+++ asynchronous/01-callback.js
@@ -15,3 +15,23 @@ app.get('/:id', (req, res) => {
    }
  });
 });
+
+
+//CALL BACKS
+function getUsers(cb){
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) return cb(err);
+    const users = JSON.parse(data);
+    return cb(null, users);
+  });
+}
+
+app.get('/', (req,res) => {
+  getUsers((err, users)=>{
+    if(err){
+      res.render('error', {error:err});
+    } else {
+      res.render('index', {title: "Users", users: users.users})
+    }
+  });
+}); 
