83ec3e7856c24fe707e86bb93a773fe0ce341a2b
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-28 23:24:14 -0400

    Update and rename 09-structure-random.js to 09-structure-api.js

--- express-api/09-structure-api.js
+++ express-api/09-structure-api.js
@@ -41,9 +41,9 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
 
-//routes.js
 const express = require('express');
 const router = express.Router();
+
 const records = require('./records');
 
 function asyncHandler(cb){
@@ -57,25 +57,29 @@ function asyncHandler(cb){
 }
 
 // Send a GET request to /quotes to READ a list of quotes
-router.get('/quotes', async (req, res)=>{
+router.get('/quotes', asyncHandler(async (req, res)=>{
     const quotes = await records.getQuotes();
     res.json(quotes);
-});
-// Send a GET request to /quotes/:id to READ(view) a quote
-router.get('/quotes/:id', async (req, res)=>{
-    try {
-        const quote = await records.getQuote(req.params.id);
-        if(quote){
-            res.json(quote);
-        } else {
-            res.status(404).json({message: "Quote not found."});
-        }
-        
-    } catch(err) {
-        res.status(500).json({message: err.message});
-    }
-});
+}));
 
+// Send a GET request to /quotes/:id to READ(view) a quote
+router.get('/quotes/:id', asyncHandler(async (req, res)=>{
+    const quote = await records.getQuote(req.params.id);
+
+    if(quote){
+        res.json(quote);
+    } else {
+        res.status(404).json({message: "Quote not found."});
+    }
+}));
+
+// Send a GET request to /quotes/quote/random to READ (view) a random quote
+router.get('/quotes/quote/random', asyncHandler(async(req,res,next) =>{
+    const quote = await records.getRandomQuote();
+    res.json(quote);
+}));
+
+//Send a POST request to /quotes to  CREATE a new quote 
 router.post('/quotes', asyncHandler( async (req, res)=>{
     if(req.body.author && req.body.quote){
         const quote = await records.createQuote({
@@ -103,20 +107,12 @@ router.put('/quotes/:id', asyncHandler(async(req,res) => {
 }));
 
 // Send a DELETE request to /quotes/:id DELETE a quote 
-router.delete("/quotes/:id", async(req,res, next) => {
-    try {
-        throw new Error("Something terrible happened!");
-        const quote = await records.getQuote(req.params.id);
-        await records.deleteQuote(quote);
-        res.status(204).end();
-    } catch(err){
-        next(err);
-    }
-});
+router.delete("/quotes/:id", asyncHandler(async(req,res, next) => {
+    const quote = await records.getQuote(req.params.id);
+    await records.deleteQuote(quote);
+    res.status(204).end();
+}));
 
-// Send a GET request to /quotes/quote/random to READ (view) a random quote
+
 
 module.exports = router; 
-
-
-   
