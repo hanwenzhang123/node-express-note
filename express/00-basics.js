7bb3bcb75485a967528f1720f0886a795ad973a1
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-09 19:47:24 -0400

    Update 00-basics.js

--- express/00-basics.js
+++ express/00-basics.js
@@ -4,23 +4,25 @@ cd myapp
 touch server.js //entry point: (server.js)
 
 npm init    //json set up
-npm install express --save    //it adds express into your json under dependencies
+
+npm install express    //it adds express into your json under dependencies, does not need '--save'
 
 
 //IDE
 const express = require('express');
-const app = express()
+
+const app = express();
 
 app.get('/', function(req, res){   //a home page, route'/', localhost:3000
-  res.send('<h1>Hello, world</h1>');   // response is what the server response to, now you can see 'Hello, world' in browser
-})    
+  res.send('<h1>Hello, world!</h1>');   // response is what the server response to, now you can see 'Hello, world' in browser
+});    
 
 app.get('/contact', function(req, res){   //we can create as many routes as we want, localhost:3000/contact
   res.send('Contact me');  
-})  
+});  
 
-app.listen(3000, function() => {
-  console.log('Server started on port 3000');
+app.listen(3000, function(){
+  console.log('Server started on port 3000.');
 });
 
 
