53db7961ce894dfe1cdec20cc2aa3cd9b2b9adbd
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-26 10:30:50 -0400

    Update 23-password.js

--- express/23-password.js
+++ express/23-password.js
@@ -1,11 +1,11 @@
 //password middleware (fake)
 app.use((req, res, next) => {
-  //if(req.path === '/secret'){   
-    const { password } = req.query;   // only this without if, it applies to any url with ?password=chickennugget
-    if (password === 'chickennugget') {
-        next();   //pass the password
+  //if(req.path === '/secret'){       //only applies to /secret?password=chickennugget
+    const { password } = req.query;   //without if, it applies to any url with ?password=chickennugget
+    if (password === 'chickennugget') {   //only if the pw is chickennugget we pass to the next middleware handler
+        next();   //pass the password to the next
     }
-    res.send("SORRY YOU NEED A PASSWORD!")
+    res.send("SORRY YOU NEED A PASSWORD!")    //if pw is not correct, you need a password
   //} 
 })
 
@@ -28,5 +28,5 @@ const verifyPassword = (req, res, next) => {
 app.get('/secret', verifyPassword, (req, res) => {    //pass the verifyPassword to first callback, here is only works on /secrete
     res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
 })
-
+      //the verifyPassword only for the secret path
   
