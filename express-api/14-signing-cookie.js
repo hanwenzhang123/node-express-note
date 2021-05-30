f39664d7d07d0a4d7c52ea0824f3150332076d2b
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-29 21:16:38 -0400

    Update 14-signing-cookie.js

--- express-api/14-signing-cookie.js
+++ express-api/14-signing-cookie.js
@@ -5,6 +5,8 @@
 //the purpose is not for hide or encrypt, but varify its integrity and something has not changed
 //make sure the original data that we send to the browser is still the data that sends back to us
 
+//HMAC, hashed based massage authentification code
+
 
 const express = require('express');
 const app = express();
@@ -24,17 +26,17 @@ app.get('/setname', (req, res) => {
 })
 
 app.get('/getsignedcookie', (req, res) => { 
-    res.cookie('fruit', 'grape', { signed: true })    //send the signed cookie where fruit is grape
+    res.cookie('fruit', 'grape', { signed: true })    //send the signed cookie where fruit is grape; however, we do not see fruit when console log
     res.send('OK SIGNED YOUR FRUIT COOKIE!')
 })
 
 app.get('/verifyfruit', (req, res) => {
     console.log(req.cookies)
     console.log(req.signedCookies)
-    res.send(req.signedCookies)   //unsign the cookie using req.signedCookie
+    res.send(req.signedCookies)   //unsign the cookie using req.signedCookie and ready for use, now we can see the fruit is grape
 })
 
 app.listen(3000, () => {
     console.log("SERVING!")
 })
- 
+  
