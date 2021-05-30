8a59fec8be16b87fd2c0440283be00af9d3a7cb5
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-29 21:12:18 -0400

    Create 14-signing-cookie.js

---
+++ express-api/14-signing-cookie.js
@@ -0,0 +1,40 @@
+//The cookie will still be visible, but it has a signature, so it can detect if the client modified the cookie.
+//If it does not match, then it will give an error.
+//instead of sending the raw value, we send signed cookie
+
+//the purpose is not for hide or encrypt, but varify its integrity and something has not changed
+//make sure the original data that we send to the browser is still the data that sends back to us
+
+
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser('thisismysecret'));    //pass a secret
+
+app.get('/greet', (req, res) => {
+    const { name = 'No-name' } = req.cookies;
+    res.send(`Hey there, ${name}`)
+})
+
+app.get('/setname', (req, res) => {
+    res.cookie('name', 'henrietta');
+    res.cookie('animal', 'harlequin shrimp')
+    res.send('OK SENT YOU A COOKIE!!!')
+})
+
+app.get('/getsignedcookie', (req, res) => { 
+    res.cookie('fruit', 'grape', { signed: true })    //send the signed cookie where fruit is grape
+    res.send('OK SIGNED YOUR FRUIT COOKIE!')
+})
+
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies)
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)   //unsign the cookie using req.signedCookie
+})
+
+app.listen(3000, () => {
+    console.log("SERVING!")
+})
+ 
