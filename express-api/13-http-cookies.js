3f53190ffacf9f5e300aa3e0c9b518cc55bb3691
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-29 20:49:19 -0400

    Update 13-http-cookies.js

--- express-api/13-http-cookies.js
+++ express-api/13-http-cookies.js
@@ -1,6 +1,7 @@
 cookie - session management, personalization (remember data), tracking
 
 
+//response with cookie
 
 const express = require('express');
 const app = express();
@@ -19,3 +20,29 @@ app.listen(3000, ()=>{
     console.log('Serving app on localhost:3000')
 })
 
+
+
+//cookie parser middleware
+//$ npm i cookie-parser
+
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());        //we execute it
+
+app.get('/greet', function(req, res){ 
+    const {name = 'noname'} = req.cookies    //req.cookies.name with a default value
+    res.send(`Hello, world ${name}`);       // Hello, world stevie chicks
+});  
+
+app.get('/setname', function(req, res){ 
+    res.cookie('name', 'stevie chicks');        //sending the cookie. now in the cookie of the browser for localhost 3000, we have the data name is stevie chicks in the cookie
+    res.send("sending chickcen");  
+});  
+
+app.listen(3000, ()=>{
+    console.log('Serving app on localhost:3000')
+})
+
+  
