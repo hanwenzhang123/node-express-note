175bf0864ffb0c3d8552138729d52ff55ad59d9c
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-05 23:30:02 -0400

    Create validation-questions.js

---
+++ express-api/validation-questions.js
@@ -0,0 +1,29 @@
+Which HTTP status response code indicates that the request has succeeded and has led to creating a resource?
+  201 Created
+
+Validating data on the _____ ensures that users can't submit bad data by bypassing client-side validation.
+database
+
+When developing full stack web applications, where can you validate data?
+  Development tools, Database, Server
+
+Validating data on the _____ ensures that users have the best possible experience. The browser provides immediate feedback as users update form fields.
+client
+
+Why is data validation important?
+As developers, we should strive to create applications that ensure that data created and updated by our users is useful.
+
+Which route returns a list of user accounts?
+  GET /api/users
+
+
+Which of the following is an example of a "consistency check" validation?
+  Check if a user's password matches their password confirmation
+  
+  What type of validation would you use to ensure that a password contains 8 characters but no more than 20 characters?
+  Which of the following is an example of a "consistency check" validation?
+  
+  Should you consider your audience when creating validation error messages?
+  Yes. Consider whether error messages are meant to be read by developers or end-users.
+
+Hashing is a technique developers use to protect a user's password before storing it in the database.
