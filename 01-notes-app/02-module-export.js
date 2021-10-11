8d638996f1c7e18ac0f615ee668c3293329c55ff
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-11 13:19:52 -0400

    Create 02-module-export.js

---
+++ 01-notes-app/02-module-export.js
@@ -0,0 +1,39 @@
+//Example 1
+//app.js
+const name= require("./utils.js");
+console.log(name);  //Hanwen
+
+//utils.js
+console.log("utils.js");
+const name = "Hanwen";
+module.exports = name;
+
+
+//Example 2
+//app.js
+const add = require("./utils.js");
+const sum = add(4, -2);
+console.log(sum);   //2
+
+//utils.js
+console.log("utils.js");
+const name = "Hanwen";
+const add = function (a, b) {
+  return a + b;
+};
+module.exports = add;
+
+
+//Example 3
+//app.js
+const getNotes = require("./notes.js");
+const msg = getNotes();
+console.log(msg);   //Your notes...
+
+//notes.js
+const getNotes = function () {
+  return "Your notes...";
+};
+module.exports = getNotes;
+
+ 
