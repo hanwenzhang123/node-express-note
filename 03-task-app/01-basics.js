06b0e07dd5f200bd610c964386d97966b1a2d934
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-13 15:18:33 -0400

    Update 01-basics.js

--- 03-task-app/01-basics.js
+++ 03-task-app/01-basics.js
@@ -1,1 +1,7 @@
+SQL vs NoSQL
+Table vs Collection
+Row/Record vs Document
+Column vs Field
 
+https://www.mongodb.com/try/download/community
+/Users/{yourname}/Documents/mongodb/bin/mongod --dbpath=/Users/{yourname}/Documents/mongodb-data
