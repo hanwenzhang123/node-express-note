a7b45979b276449b70f4e72759122afef3b5fc13
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-13 11:23:40 -0400

    Update 03-import-npm-modules.js

--- 01-notes-app/03-import-npm-modules.js
+++ 01-notes-app/03-import-npm-modules.js
@@ -1,1 +1,19 @@
 // Importing npm Modules
+//check version
+node -v
+npm -v
+
+//initialize npm
+npm init
+-> package.json (javascript object notation)
+
+//install package
+npm i {name of the package}
+npm i validator
+-> package-lock.json
+
+//use require to use npm packages
+const validator = require('validator')
+
+console.log(validator.isEmail("zhang@example.com"));  //true
+console.log(validator.isURL("https/mead.io"));    //false
