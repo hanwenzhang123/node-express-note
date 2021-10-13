1eec7b6160308ee71f6eeaa81bc9ec4704d4b4b5
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-13 11:36:20 -0400

    Update 03-import-npm-modules.js

--- 01-notes-app/03-import-npm-modules.js
+++ 01-notes-app/03-import-npm-modules.js
@@ -17,3 +17,27 @@ const validator = require('validator')
 
 console.log(validator.isEmail("zhang@example.com"));  //true
 console.log(validator.isURL("https/mead.io"));    //false
+
+//install dependencies of the application, get the folder of the node_modules back
+npm install
+
+
+//Printing in Color
+npm i chalk
+
+//app.js
+const chalk = require("chalk");
+console.log(chalk.blue.bold.underline("Hello"));
+const greenMsg = chalk.green.inverse("Success!");
+console.log(greenMsg);
+
+
+//Global npm Modules and nodemon
+//sudo stands for superuser do.
+//-g is for globally
+sudo npm install nodemon -g 
+
+//run node file automatically
+nodemon {name of the file like app.js}
+press control c to stop nodemon
+  
