d8eeaeff71204fbf01e051d1b29a82b9ae6f1b44
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-13 22:44:07 -0400

    Update 11-debugging.js

--- 01-notes-app/11-debugging.js
+++ 01-notes-app/11-debugging.js
@@ -8,10 +8,26 @@ Stack trace
 - a report of the active stack frames at a certain point in time during the execution of a program
 e.g. ReferenceError: dataJSON is not defined
 
-There are three main types of errors that can occur while compiling a JavaScript program: 
+
+//There are three main types of errors that can occur while compiling a JavaScript program: 
 - syntax errors:  incorrect in the syntax of the program body raises this error.
 - runtime errors: occur during the runtime of the program after it is interpreted by the compiler.
 - logical errors: statement is correct with respect to its syntax but is logically incorrect. 
+
+
+//Six primary error types
+EvalError: Raised when the eval() functions is used in an incorrect manner.
+RangeError: Raised when a numeric variable exceeds its allowed range.
+ReferenceError: Raised when an invalid reference is used.
+SyntaxError: Raised when a syntax error occurs while parsing JavaScript code.
+TypeError: Raised when the type of a variable is not as expected.
+strong text URIError: Raised when the encodeURI() or decodeURI() functions are used in an incorrect manner.
+
+
+//TypeError and ReferenceError with undefined
+A ReferenceError occurs when you try to use a variable that does not exist at all.
+A TypeError occurs when the variable exists, but the operation you are trying to perform is not appropriate for the type of value it contains. 
+
 
 Uncaught TypeError: Cannot read property
 TypeError: ‘undefined’ is not an object
@@ -23,15 +39,4 @@ Uncaught RangeError
 TypeError: Cannot read property ‘length’
 Uncaught TypeError: Cannot set property
 ReferenceError: event is not defined
-
-A ReferenceError occurs when you try to use a variable that does not exist at all.
-A TypeError occurs when the variable exists, but the operation you are trying to perform is not appropriate for the type of value it contains. 
-
-EvalError: Raised when the eval() functions is used in an incorrect manner.
-RangeError: Raised when a numeric variable exceeds its allowed range.
-ReferenceError: Raised when an invalid reference is used.
-SyntaxError: Raised when a syntax error occurs while parsing JavaScript code.
-TypeError: Raised when the type of a variable is not as expected.
-strong text URIError: Raised when the encodeURI() or decodeURI() functions are used in an incorrect manner.
-
-  
+   
