7e378aa419f2074f07392a5b0d22e3115e86ddc0
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-17 20:05:53 -0400

    Update 07-static-assets.js

--- 02-weather-app/07-static-assets.js
+++ 02-weather-app/07-static-assets.js
@@ -23,17 +23,47 @@ var path = require('path');
 var x = path.join(paths);
 
 
-//app.js 
-//we serve static files, so no needs app.get for every single route
-//we can just do /about.html /help.html in the path
+//src/app.js 
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')   //point to a path
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')   //point to a path
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')   //tell which templating engine you use, like handlerbar, ejs
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -42,12 +72,31 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+// app.get('*', (req, res) => {        //wildcard, always the last one
+//     res.send("My 404 page")         //send the error message
+// })
+
+app.get('*', (req, res) => {    //wildcard, always the last one
+    res.render('404', {     //render the handlerbar file
+        title: '404',
+        errorMessage: 'Page not found.'
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
 
 
-//public/index.html - the root file
+//templates/views/index.hbs - the root file
 //href - This is the path to the file we're trying to load. use relative (./css/styles.css) or absolute (/css/styles.css)
 <!DOCTYPE html>
 <html>
@@ -56,32 +105,62 @@ app.listen(3000, () => {
     <script src="/js/app.js"></script>
 </head>
 <body>
-    <h1>From a static file</h1>
+    {{>header}}
+    {{>footer}}
 </body>
 </html>
 
-//public/help.html
+//templates/views/help.hbs
 <!DOCTYPE html>
 <html>
 <head>
     <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
-    <h1>Help</h1>
+    {{>header}}
+    <p>{{helpText}}</p>
+    {{>footer}}
 </body>
 </html>
 
-//public/about.html
+//templates/views/about.hbs
 <!DOCTYPE html>
 <html>
 <head>
     <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
-    <h1>About</h1>
+    {{>header}}
     <img src="/img/me.png">
+    {{>footer}}
 </body>
 </html>
+
+//templates/views/404.hbs
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    {{>header}}
+    <p>{{errorMessage}}</p>
+    {{>footer}}
+</body>
+</html>
+
+
+//templates/partials/header.hbs
+<h1>{{title}}</h1>
+<div>
+    <a href="/">Weather</a>
+    <a href="/about">About</a>
+    <a href="/help">Help</a>
+</div>
+
+//templates/partials/footer.hbs
+<p>Created by {{name}}</p>
+
 
 //public/css/styles.css
 h1 {
@@ -93,4 +172,4 @@ img {
 
 //public/js/app.js
 console.log('Client side javascript file is loaded!')
- 
+  
