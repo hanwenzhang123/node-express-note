06b7365cf4f64f9d7f93938db6f6d6d545b11460
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-23 23:48:50 -0400

    Update 9-async-await.js

--- 05-playground/9-async-await.js
+++ 05-playground/9-async-await.js
@@ -1,8 +1,27 @@
+//async always return a promise, promise is fulfilled with the value returned from the function
+
+const doWork = async() => { }   
+console.log(doWork())   //Promise { undefined }
+
+const doWork = async() => { return "Andrew" }   
+console.log(doWork())   //Promise { "Andrew" }
+
+const doWork = async() => { 
+    throw new Error("something went wrong")
+    return "Andrew" 
+}   
+doWork().then((result) => {
+    console.log("result", result)
+}).catch(e) => {
+    console.log("e", e)     //"something went wrong"
+}
+
+//async-await.js
 const add = (a, b) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (a < 0 || b < 0) {
-                return reject('Numbers must be non-negative')
+            if (a < 0 || b < 0) {   //not passing the if check, will trigger reject
+                return reject('Numbers must be non-negative')   //stop function execution
             }
 
             resolve(a + b)
@@ -10,11 +29,11 @@ const add = (a, b) => {
     })
 }
 
-const doWork = async () => {
-    const sum = await add(1, -99)
-    const sum2 = await add(sum, 50)
-    const sum3 = await add(sum2, -3)
-    return sum3
+const doWork = async () => {        //async await, things happen one at a time 
+    const sum = await add(1, 99)       //await works like return with the promise call from add()
+    const sum2 = await add(sum, 50)     //we get value from sum, we can chain multiple await
+    const sum3 = await add(sum2, 3)    //we can use the value we can from sum2 then chain another await for updated value
+    return sum3         //return the value for 3 async action for doWork() function
 }
 
 doWork().then((result) => {
@@ -22,3 +41,4 @@ doWork().then((result) => {
 }).catch((e) => {
     console.log('e', e)
 })
+ 
