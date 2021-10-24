9c4e66cf12caf21d1d2e977d7a2b097c133016ad
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-24 15:05:13 -0400

    Create 09-promise-chaining.js

---
+++ 03-task-app/09-promise-chaining.js
@@ -0,0 +1,50 @@
+//promise-chaining.js
+require('../src/db/mongoose')
+const User = require('../src/models/user')
+
+// User.findByIdAndUpdate('5c1a5a34d5a2ec046ca8f6bc', { age: 1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('5c1a5a34d5a2ec046ca8f6bc', 2).then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
+})
+
+
+//promise-chaining-2.js
+require('../src/db/mongoose')
+const Task = require('../src/models/task')
+
+// Task.findByIdAndDelete('5c1a63e8f0d4c50656c5ab28').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5c1a634150c97706427d4661').then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
+})
