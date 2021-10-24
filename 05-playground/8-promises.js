ad3a4ccee759abf9ef50a2b9bff2d262b24ddc0a
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-23 23:15:24 -0400

    Update 8-promises.js

--- 05-playground/8-promises.js
+++ 05-playground/8-promises.js
@@ -6,23 +6,28 @@ const add = (a, b) => {
     })
 }
 
-// add(1, 2).then((sum) => {
-//     console.log(sum)
+//nested promises
 
-//     add(sum, 5).then((sum2) => {
-//         console.log(sum2)
-//     }).catch((e) => {
-//         console.log(e)
-//     })
-// }).catch((e) => {   
-//     console.log(e)
-// })
-
-add(1, 1).then((sum) => {
+add(1, 2).then((sum) => {
     console.log(sum)
-    return add(sum, 4)
-}).then((sum2) => {
-    console.log(sum2)
-}).catch((e) => {
+
+    add(sum, 5).then((sum2) => {
+        console.log(sum2)
+    }).catch((e) => {
+        console.log(e)
+    })
+}).catch((e) => {   
     console.log(e)
 })
+
+//promise chaining - you can return a promise from one of your then callbacks, allowing you to chain another then call on.
+
+add(1, 1).then((sum) => {
+    console.log(sum)        //2 - wait 2 seconds
+    return add(sum, 4)      //return next promise from .then() callback
+}).then((sum2) => {         //we chain the .then() using the return value
+    console.log(sum2)       //6 - wait 2 seconds
+}).catch((e) => {           //catch error
+    console.log(e)
+})
+ 
