b820a259a00e5936f25a30886f4dbc9a9af748a8
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-25 22:44:59 -0400

    Create 13-json-web-tokens.js

---
+++ 03-task-app/13-json-web-tokens.js
@@ -0,0 +1,7 @@
+//JSON Web Tokens(JWT) 
+- generate a token after successfully verified its authentication
+- you can set how long you want to token to be expired or not, and check if the token access for the user has expired
+https://www.npmjs.com/package/jsonwebtoken
+npm i jsonwebtoken
+
+//Generating Authentication Tokens
