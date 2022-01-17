a16e305cec96a3c841f889d364ddf77e21eedcd3
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-16 22:53:57 -0500

    Update 19-jest-testing.js

--- 03-task-app/19-jest-testing.js
+++ 03-task-app/19-jest-testing.js
@@ -2,3 +2,18 @@
 // in the package.json, add to "scripts": {"test": "jest"}
 // fileName.test.js
 // npm test
+
+test("Hello World", ( )=>{ })    //test name and test function
+
+test("This should fail", ()=>{ 
+  throw new Error("Failure!")
+})
+
+//Why test?
+// - save time
+// - create reliable software
+// - give flexibility to developers
+// -    refactoring
+// -    collaborating
+// -    profiling (check speed)
+// - peace of mind
